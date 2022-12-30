7c35490437e530b11ede15d111f994a7c57442c2
JasonMur <cjasmurph@gmail.com>
2022-12-30 15:19:17 +0000

    Update revCounter.py

--- revCounter.py
+++ revCounter.py
@@ -3,17 +3,19 @@ import lcd
 import time
 
 lcd.lcd_init()
-tacho.setGpioPin(2)
-duration = 2
+tacho.setTachoPin(2)
+tacho.setTdcPin(3)
+tacho.setStrobePin(4)
+duration = 5
 gateTime = 0.0001
 cylinders = 4
 
 while True:
-	revs, dwellAngle = tacho.countPulses(cylinders, duration, gateTime)
+	revs, advanceAngle, dwellAngle = tacho.countPulses(cylinders, duration, gateTime)
 	rpm = revs *60/duration
 	lcdString = "RPM:" + str(rpm) 
-	lcd.lcd_string(lcdString,0x80,1)
-	lcdString = "Dwell Angle:" + str(dwellAngle)
+	lcd.lcd_string(lcdString,0x80,2)
+	lcdString = "DW:" + str(dwellAngle)[0:4] + " AD:" + str(advanceAngle)[0:4]
 	lcd.lcd_string(lcdString,0xC0,1)
-	
 	time.sleep(1)
+
