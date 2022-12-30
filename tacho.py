fa63af8f6b4b94ed948ab8ae3f7cbaceb17d4806
JasonMur <cjasmurph@gmail.com>
2022-12-30 15:28:34 +0000

    Update tacho.py

--- tacho.py
+++ tacho.py
@@ -2,32 +2,68 @@ import RPi.GPIO as GPIO
 import time, sys
 
 GPIO.setmode(GPIO.BCM)
-gpioPin = 2
+tachoPin = 2
+tdcPin = 3
+strobePin = 4
 
 def about():
-	print("Tacho programme copyright Jason Industries 2022")
+	print("Tacho programme copyright Jason 2022")
 
-def setGpioPin(newGpioPin):
-	gpioPin = newGpioPin
-	GPIO.setup(gpioPin, GPIO.IN)
+def setTachoPin(newTachoPin):
+	tachoPin = newTachoPin
+	GPIO.setup(tachoPin, GPIO.IN)
+
+def setTdcPin(newTdcPin):
+        tdcPin = newTdcPin
+        GPIO.setup(tdcPin, GPIO.IN)
+
+def setStrobePin(newStrobePin):
+        strobePin = newStrobePin
+        GPIO.setup(strobePin, GPIO.OUT)
 
 def countPulses(cylinders, duration, gateTime):
 	loops = 0
 	dwellCount = 0
+	advanceCount = 0
+	advanceLoops = 0
 	revs = 0
 	cylCount = 0
+	cylinder = 0
 	startTime = time.time()
 	lastPointsSignal = 0
+	lastTdcSignal = 0
 	while  duration > (time.time()-startTime):
-		pointsSignal = GPIO.input(gpioPin)
+		pointsSignal = GPIO.input(tachoPin)
 		if pointsSignal == 0: 
 			dwellCount += 1
-		else:
 			if pointsSignal != lastPointsSignal:
 				cylCount += 1
+				advanceLoops = 0
+				if cylinder == 1:
+					GPIO.output(strobePin, True)
+					cylinder = 3
+				elif cylinder == 3:
+					cylinder = 4
+				elif cylinder == 4:
+					cylinder = 2
+				elif cylinder == 2:
+					cylinder = 1
+		else:
+			GPIO.output(strobePin, False)
 		lastPointsSignal = pointsSignal
+		tdcSignal = GPIO.input(tdcPin)
+		if tdcSignal == 0:
+			cylinder = 3
+			if tdcSignal != lastTdcSignal:
+				advanceCount += advanceLoops
+		lastTdcSignal = tdcSignal
+		advanceLoops += 1
 		loops += 1
 		time.sleep(gateTime)
+	GPIO.output(strobePin, False)
 	revs = cylCount/cylinders
+	advanceAngle = 360*advanceCount/(loops*2)
 	dwellAngle = 360*dwellCount/(cylinders*loops)
-	return revs, dwellAngle
+	return revs, advanceAngle, dwellAngle
+
+
